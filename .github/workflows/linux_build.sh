3fe406ece140c7a765336d8f39f4cb8bd55fce2d
guilhermebs <guilhermebs@drcmr.dk>
2020-07-07 12:08:35 +0200

    fixes to the CI

--- .github/workflows/linux_build.sh
+++ .github/workflows/linux_build.sh
@@ -2,7 +2,6 @@
 # This is an auxiliary script to build SimNIBS wheels
 PYTHON_PATH=$1
 $PYTHON_PATH setup.py bdist_wheel
-$PYTHON_PATH -m auditwheel repair dist/*.whl
-$PYTHON_PATH -m pip install charm-gems -f wheelhouse/
+$PYTHON_PATH -m pip install charm-gems -f dist/
 $PYTHON_PATH -c 'import charm_gems'
 rm charm_gems/*.so gems_python/*.so
--- .github/workflows/windows_build.bat
+++ .github/workflows/windows_build.bat
@@ -4,3 +4,5 @@ python -m pip install --user --upgrade setuptools wheel || exit /b
 python setup.py bdist_wheel                             || exit /b
 python -m pip install charm-gems -f .\dist\             || exit /b
 python -c 'import charm_gems'                           || exit /b
+del charm_gems\*.pyd gems_python\Releases\*.pyd         || exit /b
+
