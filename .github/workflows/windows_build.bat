2e5339e02a42305d1b59fc4979f802827785edd9
guilhermebs <guilhermebs@drcmr.dk>
2020-07-07 14:54:19 +0200

    fixed typos on windows CI

--- .github/workflows/windows_build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-python -m pip install --user --upgrade setuptools wheel || exit /b
-python setup.py bdist_wheel                             || exit /b
-python -m pip install charm-gems -f .\dist\             || exit /b
-python -c 'import charm_gems'                           || exit /b
-del charm_gems\*.pyd gems_python\Releases\*.pyd         || exit /b
-
