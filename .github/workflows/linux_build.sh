725b3d6282bd8d5f64526f008d05743e80e667d6
Oula <oulap@drcmr.dk>
2023-09-27 13:25:10 -0400

    Changed setup slightly

--- .github/workflows/linux_build.sh
+++ .github/workflows/linux_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 # This is an auxiliary script to build SimNIBS wheels
 PYTHON_PATH=$1
-$PYTHON_PATH setup.py bdist_wheel
-$PYTHON_PATH -m pip install charm-gems -f dist/
+$PYTHON_PATH -m pip wheel . -w ./dist  --no-deps
+$PYTHON_PATH -m pip install charm_gems -f dist/
 $PYTHON_PATH -c 'import charm_gems'
 rm charm_gems/*.so gems_python/*.so
